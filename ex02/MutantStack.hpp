9da9741e21e21d31c19cfcd77192456599deb84c
jordi <janesoy@hotmail.com>
2023-12-15 19:43:30 +0100

    fixx post operators ++ and --

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -19,9 +19,9 @@ class MutantStack: public std::stack<T>{
 				iterator(T* x) :p(x) {}
 				iterator(const iterator& mit) : p(mit.p) {}
 				iterator& operator++() {++p;return *this;}
-				iterator operator++(T) {iterator tmp(*this); operator++(); return tmp;}
+				iterator operator++(int) {iterator tmp(*this); operator++(); return tmp;}
 				iterator& operator--() {--p;return *this;}
-				iterator operator--(T) {iterator tmp(*this); operator--(); return tmp;}
+				iterator operator--(int) {iterator tmp(*this); operator--(); return tmp;}
 				bool operator==(const iterator& rhs) const {return p==rhs.p;}
 				bool operator!=(const iterator& rhs) const {return p!=rhs.p;}
 				T& operator*() {return *p;}
